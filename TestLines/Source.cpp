37c054ebda4a9bb4179eb090e1d50ae671aab26c
esmelusina <esmelusina@gmail.com>
2016-03-29 13:32:25 -0700

    libs

--- TestLines/Source.cpp
+++ TestLines/Source.cpp
@@ -13,15 +13,15 @@ void main()
     float    y[5]  = {100,100,100,200,200};
 
     // node paintings
-    unsigned c[5]  = {GREEN,GREEN,GREEN,CYAN,CYAN};
+    unsigned c[5]  = {RED, RED, RED, CYAN, CYAN};
    
     // adjacency matrix
     float    a[25] = {
-                        0,0,0,1,1,
-                        0,0,0,1,1,
-                        0,0,0,1,1,
-                        1,1,1,0,0,
-                        1,1,1,0,0
+                        0, 0, 0, 1, 1,
+                        0, 0, 1, 1, 1,
+                        0, 1, 0, 1, 1,
+                        1, 1, 1, 0, 0,
+                        1, 1, 1, 0, 0
                      };
 
     while (sfwl::stepContext())
--- TestLines/visualize.h
+++ TestLines/visualize.h
@@ -5,7 +5,7 @@ inline void drawGraph(unsigned count, const float *x, const float *y, const unsi
 {
     for (unsigned i = 0; i < count-1; ++i)
         for (unsigned j = i + 1; j < count; ++j)
-            if (adj[i*count + j] != 0)        
+            if (adj[i*count + j] != 0)
                 sfwl::drawLineGradient(x[i], y[i], x[j], y[j], colors[i], colors[j]);
             
     for (unsigned i = 0; i < count; ++i)
