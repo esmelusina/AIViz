329952aec3a00254ee8cb68e3feef3cc69e65b47
esmelusina <esmelusina@gmail.com>
2016-03-29 16:07:06 -0700

    initial graph stuff

--- TestLines/Source.cpp
+++ TestLines/Source.cpp
@@ -1,33 +1,51 @@
 #include "sfwl.h"
-#include "visualize.h"
+
 #include <iostream>
+
+#include "Graph.h"
+#include "visualize.h"
+
+using Node = Graph::Node;
+
 
 
 void main()
 {
     sfwl::initContext();
 
+    Graph *pGraph = new Graph();
+    Node *a = pGraph->AddNode(Vector2(100, 100));
+    Node *b = pGraph->AddNode(Vector2(150, 100));
+    Node *c = pGraph->AddNode(Vector2(200, 100));
+    Node *d = pGraph->AddNode(Vector2(150, 150));
+    Node *e = pGraph->AddNode(Vector2(100, 200));
+    Node *f = pGraph->AddNode(Vector2(150, 200));
+    Node *g = pGraph->AddNode(Vector2(200, 200));
+    Node *h = pGraph->AddNode(Vector2(300, 150));
+    Node *i = pGraph->AddNode(Vector2(250, 100));
+    Node *j = pGraph->AddNode(Vector2(300, 100));
+    Node *k = pGraph->AddNode(Vector2(350, 100));
 
-    // node positions
-    float    x[5]  = {100,200,300,100,300};
-    float    y[5]  = {100,100,100,200,200};
-
-    // node paintings
-    unsigned c[5]  = {RED, RED, RED, CYAN, CYAN};
-   
-    // adjacency matrix
-    float    a[25] = {
-                        0, 0, 0, 1, 1,
-                        0, 0, 1, 1, 1,
-                        0, 1, 0, 1, 1,
-                        1, 1, 1, 0, 0,
-                        1, 1, 1, 0, 0
-                     };
+    pGraph->ConnectNodes(a, d, 1);
+    pGraph->ConnectNodes(b, d, 1);
+    pGraph->ConnectNodes(c, d, 1);
+    pGraph->ConnectNodes(d, h, 1);
+    pGraph->ConnectNodes(d, e, 1);
+    pGraph->ConnectNodes(d, f, 1);
+    pGraph->ConnectNodes(d, g, 1);
+    pGraph->ConnectNodes(i, h, 1);
+    pGraph->ConnectNodes(j, h, 1);
+    pGraph->ConnectNodes(k, h, 1);
+    pGraph->bakeData();
 
     while (sfwl::stepContext())
     {
-        drawGraph(5,x,y,c,a);
+        //drawGraph(pGraph->size(), pGraph->getPositions(), pGraph->getAdjacencyMatrix());
+
+        pGraph->draw();
     }
+
+    delete pGraph;
 
     sfwl::termContext();
 }
